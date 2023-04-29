1ec7b8b84b34b1c603d7425eb3dd4a290b13fd33
chirag girawale <113329509+Chirag13222@users.noreply.github.com>
2023-04-29 22:42:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+let hr = document.getElementById('hour');
+let min = document.getElementById('min');
+let sec = document.getElementById('sec');
+
+function displayTime(){
+        let date = new Date ();
+
+        // Getting Hour,min sec from date
+        let hh = date.getHours();
+        let mm = date.getMinutes();
+        let ss = date.getSeconds();
+          
+
+        let hRotation = 30*hh + mm/2;
+        let mRotation = 6*mm;
+        let sRotation = 6*ss;
+
+
+        hr.style.transform = `rotate(${hRotation}deg)`;
+        min.style.transform = `rotate(${mRotation}deg)`;
+        sec.style.transform = `rotate(${sRotation}deg)`;
+
+}
+
+setInterval(displayTime,1000)
+
+
+
+// =============================Typed Js
+
+const typed = new Typed('.multiple-text',{
+    strings:['Chirag','Girawale'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
